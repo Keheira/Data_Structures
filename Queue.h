c43b46962fc693bfa8e934a75755fca8d0a97801
Keheira <Keheira@users.noreply.github.com>
2014-03-13 15:01:48 -0400

    Create Queue.h

---
+++ Queue.h
@@ -0,0 +1,35 @@
+/* 
+ * File:   Queue.h
+ * Author: keheira
+ *
+ * Created on October 8, 2013, 1:43 PM
+ */
+
+#ifndef QUEUE_H
+#define	QUEUE_H
+
+#include <iostream>
+typedef int QueueElement;
+
+struct qnode
+{
+  	QueueElement data;
+	qnode * next;
+};
+
+class Queue
+{
+   public:
+	Queue(); // create an empty Queue
+	bool empty(); //return true if Queue is empty, otherwise return false
+	void AddQ(QueueElement x); //add a new value to the back of the Queue
+	bool Front (QueueElement & x); //retrieve the data at the front of the Queue
+	void RemoveQ(); //remove the value at the front of the Queue
+	void display(); //displays the data stored in the Queue from front to back
+
+   private:
+	qnode * myfront; //pointer to the front of the queue
+	qnode * myback; //pointer to the back of the queue
+};
+
+#endif	/* QUEUE_H */
